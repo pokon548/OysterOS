478f78838d1bf97611350a5b5213f94f9968c923
Bu Kun <65808665+pokon548@users.noreply.github.com>
2024-04-02 00:43:21 +0800

    luoxiaohei: Add swap

--- machine/luoxiaohei/default.nix
+++ machine/luoxiaohei/default.nix
@@ -27,7 +27,21 @@
             content = {
               type = "filesystem";
               format = "btrfs";
+
+              subvolumes = {
+                "/swap" = {
+                  mountpoint = "/.swapvol";
+                  swap = {
+                    swapfile.size = "3G";
+                  };
+                };
+              };
               mountpoint = "/";
+              swap = {
+                swapfile = {
+                  size = "3G";
+                };
+              };
               mountOptions = [ "compress-force=zstd" "nosuid" "nodev" ];
             };
           };
