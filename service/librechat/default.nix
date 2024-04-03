6e5520b3a2dbe4d4d1a7cabc716a932bb54b7405
Bu Kun <65808665+pokon548@users.noreply.github.com>
2024-04-03 17:34:18 +0800

    service/librechat: init

---
+++ service/librechat/default.nix
@@ -0,0 +1,22 @@
+{ config, lib, pkgs, ... }:
+
+{
+  config = lib.mkIf config.prefstore.service.librechat.enable {
+    virtualisation.oci-containers.containers = {
+      librechat = {
+        image = "ghcr.io/danny-avila/librechat:v0.7.0";
+        environment = {
+          HOST = "0.0.0.0";
+          MONGO_URI = "mongodb://host.containers.internal:27017/LibreChat";
+          ENDPOINTS = "openAI,google,bingAI,gptPlugins";
+        };
+        environmentFiles = [
+          "/run/agenix/librechat-env-file"
+        ];
+        ports = [
+          "${toString cfg.port}:3080"
+        ];
+      };
+    };
+  };
+}
