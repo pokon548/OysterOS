59f51e6134448768abd4bd018b9b143fdee0cdab
Bu Kun <65808665+pokon548@users.noreply.github.com>
2024-04-08 23:54:41 +0800

    wpsoffice: Unshare network

--- desktop/application/wpsoffice/default.nix
+++ desktop/application/wpsoffice/default.nix
@@ -1,11 +1,18 @@
 { pkgs
 , config
+, lib
 , ...
 }:
 {
   home = {
     packages = with pkgs; [
-      wpsoffice-cn
+      # Unshare network for wpsoffice. Suitable for dealing sensitive documents
+      (wpsoffice-cn.overrideAttrs (old: {
+        installPhase = builtins.replaceStrings
+          [ "--replace /usr/bin $out/bin" ]
+          [ "--replace /usr/bin \"${pkgs.util-linux}/bin/unshare -n -r $out/bin\"" ]
+          old.installPhase;
+      }))
     ];
 
     global-persistence.directories = [
