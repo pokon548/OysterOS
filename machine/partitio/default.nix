911f37a8f8f6afb777c959e48ff310e46e997d7e
Bu Kun <65808665+pokon548@users.noreply.github.com>
2024-05-05 19:21:56 +0800

    machine/partitio: Apply workaround for intel sound

--- machine/partitio/default.nix
+++ machine/partitio/default.nix
@@ -98,6 +98,13 @@
   hardware.enableRedistributableFirmware = true;
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 
+  # Workaround: https://gitlab.freedesktop.org/pulseaudio/pulseaudio/-/issues/766
+  boot.extraModprobeConfig = ''
+    options snd-hda-intel model=generic
+    options snd-hda-intel dmic_detect=0
+    blacklist snd_soc_skl
+  '';
+
   # Workaround: Lenovo seems f**ked up acpi power management. Without this config,
   #             suspend (to ram / disk) will simply reboot instead of power off. :(
   systemd.sleep.extraConfig = ''
