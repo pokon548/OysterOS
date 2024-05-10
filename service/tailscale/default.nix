1dd520ee6ba35acdb73d0d6698131a9f8a1a137b
Bu Kun <65808665+pokon548@users.noreply.github.com>
2024-05-10 22:31:01 +0800

    tailscale: Reauth if the previous machine is not logout

--- service/tailscale/default.nix
+++ service/tailscale/default.nix
@@ -14,10 +14,15 @@
           status=$(${config.systemd.package}/bin/systemctl show -P StatusText tailscaled.service)
           if [[ $status != Connected* ]]; then
             tailscale up --login-server https://${config.prefstore.system.network.domain.headscale} --auth-key "file:${config.prefstore.service.tailscale.authKeyFile}"
+            if [[ $(uname -m) != $(tailscale status | grep $(tailscale ip) | ${pkgs.gawk}/bin/awk '{print $2}') ]]; then
+              tailscale logout
+              tailscale up --login-server https://${config.prefstore.system.network.domain.headscale} --auth-key "file:${config.prefstore.service.tailscale.authKeyFile}"
+            fi
           fi
         '';
         serviceConfig = {
           Type = "oneshot";
+          Restart = "on-abnormal";
         };
         path = [ config.services.tailscale.package ];
         after = [ "tailscaled.service" "post-resume.service" ];
