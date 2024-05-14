794a4db9b3d2b406a2bdcaffade84974d29d2960
Bu Kun <65808665+pokon548@users.noreply.github.com>
2024-05-14 18:18:18 +0800

    chromium: Use gtk4 and wayland by default

--- desktop/application/ungoogled-chromium/default.nix
+++ desktop/application/ungoogled-chromium/default.nix
@@ -5,6 +5,15 @@
   programs.chromium = {
     enable = true;
     package = pkgs.ungoogled-chromium;
+    commandLineArgs = [
+      "--ozone-platform-hint=auto"
+      "--ozone-platform=wayland"
+      # make it use GTK_IM_MODULE if it runs with Gtk4, so fcitx5 can work with it.
+      # (only supported by chromium/chrome at this time, not electron)
+      "--gtk-version=4"
+      # enable hardware acceleration - vulkan api
+      "--enable-features=Vulkan"
+    ];
   };
 
   home = {
