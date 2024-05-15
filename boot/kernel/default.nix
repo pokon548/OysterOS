34f6793e5ed74bed9cb86ec0898052415aa0b725
Bu Kun <65808665+pokon548@users.noreply.github.com>
2024-05-15 12:42:49 +0800

    Revert "boot/kernel: Try intel lts kernel"
    This reverts commit 814fe80b97d50dd02b9e5719f49320114fc96ab4.

--- boot/kernel/default.nix
+++ boot/kernel/default.nix
@@ -5,29 +5,6 @@
 }: {
   config = lib.mkIf config.prefstore.boot.latestKernel
     {
-      #boot.kernelPackages = pkgs.linuxPackages_latest;
-      boot.kernelPackages =
-        let
-          linux_intel_lts_pkg = { fetchFromGitHub, buildLinux, ... } @ args:
-
-            buildLinux (args // rec {
-              version = "6.6.30";
-              modDirVersion = version;
-
-              src = fetchFromGitHub {
-                owner = "intel";
-                repo = "linux-intel-lts";
-                # After the first build attempt, look for "hash mismatch" and then 2 lines below at the "got:" line.
-                # Use "sha256-....." value here.
-                rev = "lts-v${version}-linux-240508T063812Z";
-                hash = "sha256-pcEivADw/L+mpYkQkCQj2v+pU2v2gsx2jEo6f3PJ4r8=";
-              };
-              kernelPatches = [ ];
-
-              extraMeta.branch = "${version}";
-            } // (args.argsOverride or { }));
-          linux_intel_lts = pkgs.callPackage linux_intel_lts_pkg { };
-        in
-        pkgs.recurseIntoAttrs (pkgs.linuxPackagesFor linux_intel_lts);
+      boot.kernelPackages = pkgs.linuxPackages_latest;
     };
 }
